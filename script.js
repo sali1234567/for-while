cdcf6c5442dd3417f8aebb5020fb4dd47cbf0ee7
sali1234567 <sali.chuguashvili@gmail.com>
2023-08-05 17:39:10 +0400

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+console.log("With While loop:")
 let number = 9;
 let i = 1;
 while(i <= 10){
     console.log(number + " * " + i + " = " + number * i);
     i++;
+}
+
+console.log("With For loop:");
+for(number = 9, i=1; i<=10; i++){
+    console.log(number + " * " + i + " = " + number * i);
 }
